9c3e5dc978a8bcc1679c97e821c1f74df1e2f4e3
Alexia-Anna-Joseph <78249362+Alexia-Anna-Joseph@users.noreply.github.com>
2021-08-03 08:46:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,94 +1,182 @@
 var PLAY = 1;
 var END = 0;
-var gameState = 0;
+var gameState = PLAY;
 
 var ground, ground_image, invisible_ground;
 var girl, girl_running, girl_collided, girlImage;
 var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4;
 var jumpSound, dieSound, checkpointSound;
-var score,life=3
+var zombie;
+var score;
 var gameOver, restart, gameOverImage, restartImage;
-var zombie,zombieImg;
-var heart1,heart2,heart3,heartImg;
-var game,player,form;
-var playerCount=0;
-var allPlayers=[];
-var character=[];
-
 
 function preload() {
-  ground_image = loadImage("Background.png");
+  ground_image = loadImage("images/Background.png");
   girl_running = loadAnimation(
-    "Run (1).png",
-    "Run (2).png",
-    "Run (3).png",
-    "Run (4).png",
-    "Run (5).png",
-    "Run (6).png",
-    "Run (7).png",
-    "Run (8).png",
-    "Run (9).png",
-    "Run (10).png",
-    "Run (11).png",
-    "Run (12).png",
-    "Run (14).png",
-    "Run (15).png",
-    "Run (16).png",
-    "Run (17).png",
-    "Run (18).png",
-    "Run (19).png",
-    "Run (20).png"
-    
+    "images/Run (1).png",
+    "images/Run (2).png",
+    "images/Run (3).png",
+    "images/Run (4).png",
+    "images/Run (5).png",
+    "images/Run (6).png",
+    "images/Run (7).png",
+    "images/Run (8).png",
+    "images/Run (9).png",
+    "images/Run (10).png",
+    "images/Run (11).png",
+    "images/Run (12).png",
+    "images/Run (14).png",
+    "images/Run (15).png",
+    "images/Run (16).png",
+    "images/Run (17).png",
+    "images/Run (18).png",
+    "images/Run (19).png",
+    "images/Run (20).png"
   );
-  zombieImg = loadImage("Stand.png")
-  obstacle1 = loadImage("obstacle1.png");
-  jumpSound = loadSound("jump.mp3");
-  dieSound = loadSound("die.mp3");
-  checkPointSound = loadSound("checkPoint.mp3");
-  gameOverImage = loadImage("gameOver1.png");
-  restartImage = loadImage("restart1.png");
-  girl_collided = loadImage("Dead (30).png");
-  girlImage = loadImage("Dead (30).png");
-  girl_dieImage = loadAnimation("Dead (30).png")
-  heartImg = loadImage("heart.png")
-  
+
+  obstacle1 = loadImage("images/obstacle1.png ");
+  jumpSound = loadSound("images/jump.mp3");
+  dieSound = loadSound("images/die.mp3");
+  checkPointSound = loadSound("images/checkPoint.mp3");
+  gameOverImage = loadImage("images/gameOver1.png");
+  restartImage = loadImage("images/restart1.png");
+  girl_collided = loadImage("images/dead.png");
+  girlImage = loadImage("images/dead.png");
+  zombieImage = loadImage("images/zombie.png")
 }
 
 function setup() {
-  createCanvas(displayWidth, displayHeight);
+  createCanvas(900, 500);
 
-  
+  //ground
+  ground = createSprite(0, 0, 0, 0);
+  ground.shapeColor = "white";
+  ground.addImage("ground_image", ground_image);
+  ground.scale = 1.4;
+  ground.velocityX = -1;
 
-  database = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
+  //zombie
+  zombie = createSprite(100, 420, 600, 10);
+  zombie.addAnimation("zombieImage", zombieImage);
+  //girl.addImage("girl_collided", girl_collided);
+  //girl.addImage("girlImage", girlImage);
+  zombie.scale = 0.2;
 
 
-  
+  //girl
+  girl = createSprite(300, 420, 600, 10);
+  girl.addAnimation("girl_running", girl_running);
+  girl.addImage("girl_collided", girl_collided);
+  girl.addImage("girlImage", girlImage);
+  girl.scale = 0.2;
+  // girl.velocityX=2;
+  girl.debug = false;
+  girl.setCollider("rectangle", 0, 0, girl.width, girl.height);
+
+  invisible_ground = createSprite(300, 470, 600, 10);
+  invisible_ground.visible = false;
+
+  gameOver = createSprite(400, 150);
+  gameOver.addImage(gameOverImage);
+
+  restart = createSprite(400, 250);
+  restart.addImage(restartImage);
+
+  obstaclesGroup = new Group();
+
+  score = 0;
 }
 
 function draw() {
   background("white");
 
-  if(playerCount === 1){
-    game.update(1);
+  // console.log(girl.y);
+  //Gravity
+  girl.velocityY = girl.velocityY + 0.8;
+  girl.collide(invisible_ground);
+
+  zombie.velocityY = zombie.velocityY + 0.8;
+  zombie.collide(invisible_ground);
+
+  //Gravity
+
+  if (gameState === PLAY) {
+    gameOver.visible = false;
+    restart.visible = false;
+
+    score = score + Math.round(getFrameRate() / 60);
+
+    spawnObstacles();
+
+    ground.velocityX = -(4 + (3 * score) / 100);
+
+    if (ground.x < 0) {
+      ground.x = ground.width / 2;
+    }
+
+    if (score > 0 && score % 100 === 0) {
+      checkPointSound.play();
+    }
+
+    if (keyDown("space") && girl.y >= 400) {
+      girl.velocityY = -12;
+      jumpSound.play();
+    }
+
+    if (girl.isTouching(obstaclesGroup)) {
+      girl.changeAnimation("girlImage");
+      gameState = END;
+      dieSound.play();
+    }
+
+    if (zombie.isTouching(obstaclesGroup)) {
+     zombie.velocityY = -10
+    }
+
+  } else if (gameState === END) {
+    gameOver.visible = true;
+    restart.visible = true;
+    ground.velocityX = 0;
+    girl.velocityY = 0;
+    girl.changeImage("girlImage", girlImage);
+
+    
+
+    //set lifetime of the game objects so that they are never destroyed
+    obstaclesGroup.setLifetimeEach(-1);
+    obstaclesGroup.setVelocityXEach(0);
+
+    if (mousePressedOver(restart)) {
+      reset();
+    }
   }
-  if(gameState === 1){
-    clear();
-    game.play();
-  }
-  if(gameState === 2){
-    game.end();
-  }
 
-  
-
-
- 
-
-  //fill("pink");
-  //textSize(20);
- // text("Life: " + life, 450, 250);
+  drawSprites();
+  fill("purple");
+  textSize(20);
+  text("Score: " + score, 450, 50);
 }
 
+function reset() {
+  gameState = PLAY;
+  gameOver.visible = false;
+  restart.visible = false;
+  girl.changeAnimation("girl_running", girl_running);
+  obstaclesGroup.destroyEach();
+  score = 0;
+}
+
+function spawnObstacles() {
+  if (frameCount % 60 === 0) {
+    var obstacle = createSprite(600, 450, 10, 40);
+    obstacle.velocityX = -6; //+ score/100);
+
+    //generate random obstacles
+    var rand = Math.round(random(1, 6));
+    obstacle.addImage(obstacle1);
+    obstacle.scale = 0.1;
+    obstaclesGroup.add(obstacle);
+    obstacle.debug = false;
+    obstacle.setCollider("circle", 0, 0, 1);
+  }
+}
